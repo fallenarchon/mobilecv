a0db940b51a95550a015cac4dadbdcda693b455d
fallenarchon <joeamrhein@gmail.com>
2015-10-26 14:52:25 -0700

    Add transition animations to MobileCVActivity

--- library/src/main/java/android/joeamrhein/library/MobileCVActivity.java
+++ library/src/main/java/android/joeamrhein/library/MobileCVActivity.java
@@ -5,14 +5,17 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.transition.Slide;
 import android.util.Log;
 import android.widget.ImageView;
 
@@ -50,7 +53,11 @@ public class MobileCVActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initActivityTransitions();
         setContentView(R.layout.activity_mobile_cv);
+
+        ViewCompat.setTransitionName(findViewById(R.id.app_bar_layout), EXTRA_NAME);
+        supportPostponeEnterTransition();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -105,12 +112,22 @@ public class MobileCVActivity extends AppCompatActivity {
     }
 
 
+    private void initActivityTransitions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Slide transition = new Slide();
+            transition.excludeTarget(android.R.id.statusBarBackground, true);
+            getWindow().setEnterTransition(transition);
+            getWindow().setReturnTransition(transition);
+        }
+    }
+
+
     private void applyPalette(Palette palette) {
         Resources resources = getResources();
         int primaryDark = resources.getColor(android.R.color.black);
         int primary = resources.getColor(android.R.color.darker_gray);
         mCollapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(primary));
         mCollapsingToolbarLayout.setStatusBarScrimColor(palette.getDarkMutedColor(primaryDark));
-        //        supportStartPostponedEnterTransition();
+        supportStartPostponedEnterTransition();
     }
 }
